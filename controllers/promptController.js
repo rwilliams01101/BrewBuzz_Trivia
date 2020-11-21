a60ff89975e6f8544ade2db91353a2e7b7b572e1
RAW <rwilliams01101@gmail.com>
2020-11-21 14:10:57 -0600

    updated stuff for StAD 2020

--- controllers/promptController.js
+++ controllers/promptController.js
@@ -1,37 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 // Requiring our Todo model
 const db = require("../models");
 
-router.post("/api/new", function(req, res) {
+router.post("/api/new", function (req, res) {
   var answer = req.body.answer.toLowerCase();
   db.Prompt.create({
     text: req.body.text,
-    answer: answer
+    answer: answer,
   })
-  .then((prompt) => {
-    res.json(prompt)
-  })
-  .catch((error) => {
-    res.status(500).json(error)
-  })
+    .then((prompt) => {
+      res.json(prompt);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
-
-
-
-// router.get("/api/random", function(req, res) {
-//   db.Prompt.findOne({
-//     order: [
-//       Sequelize.fn( 'RAND' ),
-//     ]
-//   })
-//   .then((prompt) => {
-//     res.send(prompt);
-//   })
-//   .catch((error) => {
-//     res.status(500).json(error)
-//   })
-// });
 
 module.exports = router;
